0b576e845b87eb6af1ce3f0a6200f9f361b0439f
Andrey Larikov <larikov174@gmail.com>
2022-02-02 20:12:39 +0500

    dev: add movies controller, modify movies routes

---
+++ controllers/movies.js
@@ -0,0 +1,55 @@
+const Movie = require('../models/movie');
+const CustomError = require('../utils/custom-error');
+
+module.exports.getMovies = (req, res, next) => {
+  Movie.find({})
+    .populate(['owner'])
+    // TODO: определить логику сортировки
+    .then((movie) => res.status(200).send(movie.sort((a, b) => b.createdAt - a.createdAt)))
+    .catch(next);
+};
+
+module.exports.createMovie = (req, res, next) => {
+  const {
+    country, director, duration, year, description, image, trailer, nameRU, nameEN, thumbnail, movieId,
+  } = req.body;
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailer,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+    owner: req.user._id,
+  })
+    .then(() => Movie.findById(movieId)
+      .populate(['owner'])
+      .orFail(new CustomError(404, 'Карточка по указанному _id не найдена'))
+      .then((newMovie) => res.status(200).send(newMovie)))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new CustomError(400, 'Переданы невалидные данные.'));
+      } else { next(err); }
+    });
+};
+
+module.exports.deleteMovie = (req, res, next) => {
+  Movie.findById(req.params.id)
+    .orFail(new CustomError(404, 'Карточка по указанному _id не найдена'))
+    .then((movie) => {
+      if (movie.owner.equals(req.user._id)) {
+        Movie.findByIdAndDelete(req.params.id)
+          .populate(['owner'])
+          .orFail(new CustomError(404, 'Карточка по указанному _id не найдена'))
+          .then((deletedCard) => res.status(200).send(deletedCard));
+      } else {
+        throw new CustomError(403, 'У вас нет прав на это действие');
+      }
+    })
+    .catch(next);
+};
