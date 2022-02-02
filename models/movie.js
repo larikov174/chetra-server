32e97cb9e2b7f471fc5dc14dd875e29e6f52fd0f
Andrey Larikov <larikov174@gmail.com>
2022-02-02 16:36:15 +0500

    dev: add user and movie schema

---
+++ models/movie.js
@@ -0,0 +1,105 @@
+const mongoose = require('mongoose');
+const isURL = require('validator/lib/isURL');
+
+const cardSchema = new mongoose.Schema(
+  {
+    country: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
+      default: 'Страна',
+    },
+    director: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
+      default: 'Режисер фильма',
+    },
+    duration: {
+      type: Number,
+      min: 0,
+      // TODO: уточнить единицы измерения и верхний предел
+      max: 300,
+      required: true,
+      default: 120,
+    },
+    year: {
+      type: String,
+      minlength: 2,
+      maxlength: 4,
+      required: true,
+      default: 'Дата',
+    },
+    description: {
+      type: String,
+      minlength: 1,
+      // TODO: уточнить макс.длину поля
+      maxlength: 300,
+      required: true,
+      default: 'Описание фильма',
+    },
+    image: {
+      type: String,
+      lowercase: true,
+      validate: {
+        validator(v) {
+          return isURL(v);
+        },
+      },
+      default:
+        '../static/default-poster.png',
+    },
+    trailerLink: {
+      type: String,
+      lowercase: true,
+      required: true,
+      validate: {
+        validator(v) {
+          return isURL(v);
+        },
+      },
+      default: 'https://www.youtube.com/',
+    },
+    thumbnail: {
+      type: String,
+      lowercase: true,
+      validate: {
+        validator(v) {
+          return isURL(v);
+        },
+      },
+      default:
+        '../static/default-poster.png',
+    },
+    owner: {
+      type: mongoose.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    movieId: {
+      type: mongoose.Types.ObjectId,
+      // TODO: проверить ссылку
+      ref: 'user',
+      required: true,
+    },
+    nameRU: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
+      default: 'Название фильма',
+    },
+    nameEN: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
+      default: 'Movie title',
+    },
+  },
+  { versionKey: false },
+);
+
+module.exports = mongoose.model('card', cardSchema);
---
+++ utils/const.js
@@ -0,0 +1,2 @@
+module.exports.db = 'mongodb://localhost:27017/filmsdb';
+module.exports.regExp = /(https?:\/\/(www\.)?)[\w-]+\.[\w./():,-]+/;
