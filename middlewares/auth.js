2e9e975447e0ba8579c54018d43c3ef51b432a12
Andrey Larikov <larikov174@gmail.com>
2022-02-02 20:26:34 +0500

    dev: add middlewares

---
+++ middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const CustomError = require('../utils/custom-error');
+
+const { JWT_SECRET = 'dev-secret' } = process.env;
+
+module.exports = (req, res, next) => {
+  const token = req.cookies.jwt;
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    throw new CustomError(401, 'Необходима авторизация');
+  }
+
+  req.user = payload;
+  next();
+};
