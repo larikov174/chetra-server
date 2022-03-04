90ad4bf105f9a77ad08f2bfbccd39202a9e64ed3
Andrey Larikov <larikov174@gmail.com>
2022-03-04 22:35:29 +0500

    feat: add token check

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,21 @@ const router = require('express').Router();
 const userRouter = require('./users');
 const movieRouter = require('./movies');
 const auth = require('../middlewares/auth');
-const { login, logout, createUser } = require('../controllers/users');
+const {
+  login,
+  logout,
+  createUser,
+  checkToken,
+} = require('../controllers/users');
 const { loginValidation, createUserValidation } = require('../middlewares/validation');
 const CustomError = require('../middlewares/custom-error-router');
 const { errorMessage } = require('../utils/const');
 
 router.post('/signin', loginValidation, login);
 router.post('/signup', createUserValidation, createUser);
-router.get('/signout', logout);
 router.use(auth);
+router.get('/signout', logout);
+router.get('/check', checkToken);
 router.use('/users', userRouter);
 router.use('/movies', movieRouter);
 router.use('*', () => {
